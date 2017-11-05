a48f20a28ffb48fd6bdc01d7b70cc8d3179b4b81
ibrobabs <babskolawole@gmail.com>
2017-11-04 20:51:57 -0600

    update

--- cheapcrm/settings/production.py
+++ cheapcrm/settings/production.py
@@ -11,7 +11,7 @@ TEMPLATE = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
