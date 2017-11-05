c3897a03c85da18c5edd21b7191132b7137cc415
ibrobabs <babskolawole@gmail.com>
2017-11-04 23:49:37 -0600

    update

--- cheapcrm/urls.py
+++ cheapcrm/urls.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 
 from django.conf import settings
 from django.conf.urls.static import static
+from marketing.views import HomePage
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+     url(r'^$', HomePage.as_view(), name='home')
 ]
 
 
