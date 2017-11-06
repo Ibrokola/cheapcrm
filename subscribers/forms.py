7727159e774eb4eacfa2c0cf5fa653853969f58c
ibrobabs <babskolawole@gmail.com>
2017-11-05 19:16:24 -0700

    update-subscribers application almost done

--- subscribers/forms.py
+++ subscribers/forms.py
@@ -2,16 +2,37 @@ from django.contrib.auth import get_user_model
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
+from .models import Subscriber
+
+class AddressMixin(forms.ModelForm):
+    class Meta:
+        model = Subscriber
+        fields = ('address_one', 'address_two', 'city', 'province')
+        widgets = {
+            'address_one':forms.TextInput(attrs={'class':'form-control'}),
+            'address_two':forms.TextInput(attrs={'class':'form-control'}),
+            'city': forms.TextInput(attrs={'class':'form-control'}),
+            'province': forms.TextInput(attrs={'class':'form-control'}),
+        }
 
 
-class SubscriberForm(UserCreationForm):
+class SubscriberForm(AddressMixin,UserCreationForm):
     
     class Meta:
-        fields = ('email','username','password1','password2')
         model = get_user_model()
-
+        fields = ('first_name','last_name','email','username','password1','password2')
+        widgets = {
+            'first_name':forms.TextInput(attrs={'class':'form-control'}),
+            'last_name':forms.TextInput(attrs={'class':'form-control'}),
+            'email': forms.TextInput(attrs={'class':'form-control'}),
+            'username': forms.TextInput(attrs={'class':'form-control'}),
+            'password1': forms.TextInput(attrs={'class':'form-control'}),
+            'password2': forms.TextInput(attrs={'class':'form-control'}),
+        }
 
     def __init__(self,*args,**kwargs):
         super().__init__(*args,**kwargs)
+        self.fields['first_name'] = 'First Name'
+        self.fields['last_name'] = 'Last Name'
         self.fields['username'].label = 'Display Name'
         self.fields['email'].label = 'Email Address'
--- subscribers/models.py
+++ subscribers/models.py
@@ -1,10 +1,24 @@
 from django.db import models
 from django.contrib import auth
+from django.contrib.auth.models import User
 
 
-
-class User(auth.models.User,auth.models.PermissionsMixin):
+class UserNew(auth.models.User,auth.models.PermissionsMixin):
     def __str__(self):
         return self.username
 
 
+
+class Subscriber(models.Model):
+    user_sub = models.ForeignKey(User)
+    address_one = models.CharField(max_length=100)
+    address_two = models.CharField(max_length=100, blank=True)
+    city = models.CharField(max_length=50)
+    province = models.CharField(max_length=2)
+    stripe_id = models.CharField(max_length=30, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'subscribers'
+
+    def __str__(self):
+        return u"%s's Subscription Info" % self.user_sub
