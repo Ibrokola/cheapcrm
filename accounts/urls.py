c5dd0108f56b80e8728f81fc9f04d7a44c7c0223
ibrobabs <babskolawole@gmail.com>
2017-11-07 03:35:02 -0700

    update - new account url created

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
-from .views import AccountList, account_detail
+from .views import AccountList, account_detail, account_cru
 
 
 urlpatterns = [
-    url(r'list/$', AccountList.as_view(), name='account_list'),
+    url(r'l/$', AccountList.as_view(), name='account_list'),
     url(r'p/(?P<uuid>[\w-]+)/$', account_detail, name='account_detail'),
+    url(r'new/$', account_cru, name='account_new'),
 ]
