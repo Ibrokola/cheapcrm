b83c8da5de5b3b0ee135af3b72eee7589648e383
ibrobabs <babskolawole@gmail.com>
2017-11-09 15:51:57 -0700

    update

--- communications/urls.py
+++ communications/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
-from .views import comm_cru, comm_detail 
+from .views import comm_cru, comm_detail, CommDelete 
 
 
 urlpatterns = [
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'd/(?P<uuid>[\w-]+)/$', comm_detail, name='comm_detail'),
     # url(r'c/(?P<uuid>[\w-]+)/$', contact_detail, name='contact_detail'),
     url(r'edit/(?P<uuid>[\w-]+)/$', comm_cru, name='comm_update'),
-    # url(r'c/delete/(?P<pk>[\w-]+)/$', ContactDelete.as_view(), name='contact_delete'),
+    url(r'delete/(?P<pk>[\w-]+)/$', CommDelete.as_view(), name='comm_delete'),
 ]
