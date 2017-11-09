40c40c9256d03886972cf1c40f0582539669fd61
ibrobabs <babskolawole@gmail.com>
2017-11-08 17:40:03 -0700

    update - update ability added

--- contacts/urls.py
+++ contacts/urls.py
@@ -5,7 +5,8 @@ from .views import contact_detail, contact_cru
 
 urlpatterns = [
     # url(r'l/$', AccountList.as_view(), name='contact_list'),
+    url(r'c/new/$', contact_cru, name='contact_new'),
     url(r'c/(?P<uuid>[\w-]+)/$', contact_detail, name='contact_detail'),
-    url(r'c/new/$', contact_cru, name='contact_cru'),
+    url(r'c/edit/(?P<uuid>[\w-]+)/$', contact_cru, name='contact_update'),
     # url(r'edit/(?P<uuid>[\w-]+)/$', account_cru, name='account_update'),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponseForbidden
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
@@ -8,44 +8,52 @@ from .forms import ContactForm
 
 
 @login_required()
-def contact_cru(request):
+def contact_cru(request, uuid=None, account=None):
     
+    #if the contact object already exists
+    if uuid:
+        contact = get_object_or_404(Contact, uuid=uuid)
+        # Contacts can only be accessed by account owner
+        if contact.owner != request.user:
+            return HttpResponseForbidden()
+    else:
+        contact = Contact(owner=request.user)
+
+
     if request.POST:
-        form = ContactForm(request.POST)
+        form = ContactForm(request.POST, instance=contact)
         if form.is_valid():
             # make sure the user owns the account
             account = form.cleaned_data['account']
             if account.owner != request.user:
                 return HttpResponseForbidden()
 
-            contact = form.save(commit=False)
-            contact.owner = request.user
-            contact.save()
+            # contact = form.save(commit=False)
+            # contact.owner = request.user
+            form.save()
             # return the user to the account detail view
             reverse_url = reverse(
                 'accounts:account_detail', args=(account.uuid,)
             )
             return HttpResponseRedirect(reverse_url)
+        else:
+            # If the form isn't valid, still fetch the account so it can be passed into the template
+            account = form.cleaned_data['account']
     else:
-        form = ContactForm()
+        form = ContactForm(instance=contact)
+
+    # this fetches the account if it exists as a URL parameter
+    if request.GET.get('account', ''):
+        account = Account.objects.get(id=request.GET.get('account', ''))
 
     context ={
         'form': form,
+        'contact': contact,
+        'account': account
     }
     template = 'contacts/contact_cru.html'
 
     return render(request, template, context)
-
-
-
-
-
-
-
-
-
-
-
 
 
 
